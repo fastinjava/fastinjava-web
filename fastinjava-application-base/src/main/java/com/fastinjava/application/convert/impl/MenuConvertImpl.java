093d4a16c3b8b3ae493fc60bcba951270fe9a152
fastinjava <13761809951@163.com>
2021-03-01 21:15:08 +0800

    c

--- fastinjava-application-base/src/main/java/com/fastinjava/application/convert/impl/MenuConvertImpl.java
+++ fastinjava-application-base/src/main/java/com/fastinjava/application/convert/impl/MenuConvertImpl.java
@@ -2,7 +2,6 @@ package com.fastinjava.application.convert.impl;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.ObjectUtil;
-import cn.hutool.core.util.StrUtil;
 import com.fastdevelopinjava.framework.system.api.dto.MenuDTO;
 import com.fastdevelopinjava.framework.system.api.dto.MenuInsertDTO;
 import com.fastdevelopinjava.framework.system.api.dto.MenuReqDTO;
@@ -13,33 +12,32 @@ import com.fastinjava.framework.baseapplication.vo.MenuReqVO;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MenuConvertImpl implements MenuConvert {
+public class MenuConvertIMpl implements MenuConvert {
     @Override
     public MenuListDetailVO menuDTO2MenuListDetailVO(MenuDTO menuDTO) {
-        if (ObjectUtil.isEmpty(menuDTO)) return null;
+        if (ObjectUtil.isEmpty(menuDTO)) {
+            return null;
+        }
         MenuListDetailVO menuListDetailVO = new MenuListDetailVO();
         BeanUtil.copyProperties(menuDTO,menuListDetailVO);
         return menuListDetailVO;
     }
 
-    /**
-     * MenuReqVO menuReqVO ----> MenuReqDTO
-     *
-     * @param menuReqVO
-     */
     @Override
-    public MenuReqDTO menuReqVO2MenuReqDTO(MenuReqVO menuReqVO) {
+    public MenuInsertDTO menuInsertVO2MenuInsertDTO(MenuInsertVO menuInsertVO) {
+        if (ObjectUtil.isEmpty(menuInsertVO)) {
+            return null;
+        }
+        MenuInsertDTO menuInsertDTO = new MenuInsertDTO();
+        BeanUtil.copyProperties(menuInsertVO,menuInsertDTO);
+        return menuInsertDTO;
+    }
+
+    @Override
+    public MenuReqDTO menuReqVO2MenuListDetailVO(MenuReqVO menuReqVO) {
         if (ObjectUtil.isEmpty(menuReqVO)) return null;
         MenuReqDTO menuReqDTO = new MenuReqDTO();
         BeanUtil.copyProperties(menuReqVO,menuReqDTO);
         return menuReqDTO;
-    }
-
-    @Override
-    public MenuInsertDTO menuInsertVO2MenuInsertDTO(MenuInsertVO menuInsertVO) {
-        if (ObjectUtil.isEmpty(menuInsertVO)) return null;
-        MenuInsertDTO menuInsertDTO = new MenuInsertDTO();
-        BeanUtil.copyProperties(menuInsertVO,menuInsertDTO);
-        return menuInsertDTO;
     }
 }
