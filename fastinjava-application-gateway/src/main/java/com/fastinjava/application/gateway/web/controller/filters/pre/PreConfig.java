5dbb2dda0620e771c0642ce9338e466b9292c699
fastinjava <13761809951@163.com>
2021-04-15 00:52:35 +0800

    动态路由的制作

--- fastinjava-application-gateway/src/main/java/com/fastinjava/application/gateway/web/controller/filters/pre/PreConfig.java
+++ fastinjava-application-gateway/src/main/java/com/fastinjava/application/gateway/web/controller/filters/pre/PreConfig.java
@@ -1,20 +1,44 @@
-//package com.fastinjava.application.gateway.web.controller.filters.pre;
-//
-//import org.springframework.cloud.gateway.route.Route;
-//import org.springframework.cloud.gateway.route.RouteLocator;
-//import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
-//import org.springframework.cloud.gateway.route.builder.PredicateSpec;
-//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-//import org.springframework.cloud.gateway.route.builder.UriSpec;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.util.function.Function;
-//
-//@Configuration
-//public class PreConfig {
-//
-//
+package com.fastinjava.application.gateway.web.controller.filters.pre;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.route.builder.UriSpec;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+@Configuration
+public class PreConfig {
+
+    @Bean
+    public RouteLocator customerRouteLocator(RouteLocatorBuilder builder) {
+        RouteLocatorBuilder.Builder routerBuilder = builder.routes();
+        routerBuilder.route(predicateSpec -> predicateSpec.path("/api/demo1/**").filters(new Function<GatewayFilterSpec, UriSpec>() {
+            @Override
+            public UriSpec apply(GatewayFilterSpec gatewayFilterSpec) {
+                gatewayFilterSpec.stripPrefix(1);
+                gatewayFilterSpec.filters(
+                        new FJAddRequestParameterFilter(new FJAddRequestParameterFilter.Config("flag","1"))
+                );
+                return gatewayFilterSpec;
+            }
+        }).uri("http://localhost:50001").id("demo1"));
+
+        routerBuilder.route(predicateSpec -> predicateSpec.path("/api/fastdevelopinjava-service-ucenter/**").filters(new Function<GatewayFilterSpec, UriSpec>() {
+            @Override
+            public UriSpec apply(GatewayFilterSpec gatewayFilterSpec) {
+                gatewayFilterSpec.stripPrefix(1);
+                gatewayFilterSpec.filters(
+                        new FJAddRequestParameterFilter(new FJAddRequestParameterFilter.Config("flag","1"))
+                );
+                return gatewayFilterSpec;
+            }
+        }).uri("lb://fastdevelopinjava-service-ucenter").id("fastdevelopinjava-service-ucenter"));
+
+        return routerBuilder.build();
+    }
 //    @Bean
 //    public RouteLocator customerRouteLocator(RouteLocatorBuilder builder) {
 //        return builder.routes().route(
@@ -40,5 +64,5 @@
 //
 //        ).build();
 //    }
-//
-//}
+
+}
